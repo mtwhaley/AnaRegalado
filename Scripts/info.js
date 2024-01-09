38b7cc6f846772b4e0d384e283f980df05b80e58
mtwhaley <mitchell.t.whaley@gmail.com>
2024-01-08 21:35:04 -0600

    js files condensed

--- Scripts/info.js
+++ Scripts/info.js
@@ -1,4 +1,16 @@
+const servicesOffered=[
+    "ADHD","Anger Management", "Behavioral Issues", "Bipolar Disorder",
+    "Chronic Illness", "Coping Skills", "Domestic Abuse", "Domestic Violence","Eating Disorders",
+    "Family Conflict", "Grief", "Marital and Premarital Conflict", "Prenatal/ Postpartum/ Maternal Issues",
+    "Racial Identity", "Relationship Issues", "School Issues", "Self Esteem Issues", "Sleep or Insomnia",
+    "Spirituality Issues", "Stress", "Womens' Issues", "Anxiety"
+]
 
+const approaches=[
+    "Art Therapy", "Christian Counseling", "Cognitive Behavioral Therapy (CBT)", "Culturally-Sensitive Therapy",
+    "Dialectal Behavior Therapy(DBT)", "Family Systems", "Motivational Interviewing", "Person-Centered Therapy",
+    "Solution Focused Brief Therapy (SFBT)", "Strength-Based Therapy","Trauma-Focused Therapy"
+]
 
 
 function loadListTable(tableID, numCols, array) {
